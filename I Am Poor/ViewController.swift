466d010a496f102e212b01d071a9c912c1abd1a6
Elphan-gs <johnchristianmgaron@gmail.com>
2021-07-08 17:44:57 +0800

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by John Christian Garon on 7/8/21.
+//  Copyright © 2021 John Christian Garon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
